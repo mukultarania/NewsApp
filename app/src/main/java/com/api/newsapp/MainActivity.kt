b66b3efcce8aaed5e84dbcd12ced4a0ee0ec1648
mukul <taraniamukul@gmail.com>
2021-09-06 08:55:00 +0530

    Final Commit

--- app/src/main/java/com/api/newsapp/MainActivity.kt
+++ app/src/main/java/com/api/newsapp/MainActivity.kt
@@ -1,30 +1,60 @@
 package com.api.newsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), NewsItemClicked {
+
+    private lateinit var mAdapter: NewsListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler.layoutManager = LinearLayoutManager(this)
-        val item = fetchData()
-        val adapter: NewsListAdapter = NewsListAdapter(item, this)
-        recycler.adapter = adapter
+//        fetchData()
+//        mAdapter = NewsListAdapter(this)
+//        recycler.adapter = mAdapter
+        Toast.makeText(this, "sssssssssssssssssssssssss", Toast.LENGTH_SHORT)
+
     }
 
-    private fun fetchData(): ArrayList<String> {
-        val list = ArrayList<String>()
-        for(i in 0 until 100){
-            list.add("Item $i")
-        }
-        return list
+    private fun fetchData() {
+        val newsArray = ArrayList<News>()
+        val url =
+            "https://newsapi.org/v2/top-headlines?country=in&apiKey=533a150c941c47f4bff6fe597da02bba"
+        val jsonObject = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            {
+                val newsJSONArray = it.getJSONArray("articles")
+                for (i in 0 until newsJSONArray.length()) {
+                    val newJsonObject = newsJSONArray.getJSONObject(i)
+                    val news = News(
+                        newJsonObject.getString("title"),
+                        newJsonObject.getString("author"),
+                        newJsonObject.getString("url"),
+                        newJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                    Toast.makeText(this, "Working", Toast.LENGTH_SHORT)
+                }
+                mAdapter.upadateNews(newsArray)
+
+            },
+            {
+                Toast.makeText(this, "Not Working", Toast.LENGTH_SHORT)
+            }
+        )
+        NewsSingleton.getInstance(this).addToRequestQueue(jsonObject)
     }
 
-    override fun onItemClicked(item: String) {
-        Toast.makeText(this, "Item clicked is $item", Toast.LENGTH_SHORT).show()
+    override fun onItemClicked(item: News) {
+        TODO("Not yet implemented")
     }
 }
